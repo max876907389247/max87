e15c7f455980f15269ef267935b239d657738074
max876907389247 <96043140+max876907389247@users.noreply.github.com>
2021-12-13 12:17:02 +0500

    new sait

---
+++ Витос/st/mail.php
@@ -0,0 +1,15 @@
+$name = $_Post['user_name'];
+$name = $_Post['user_phone'];
+$name = $_Post['user_email'];
+
+$mail → isSMTP();
+$mail → Host = 'smtp.mail.ru';
+
+$mail → SMTAuth = 'true';
+$mail → Username = 'maxxx899ttr@mail.ru';
+$mail → Password = '12max23max';
+
+$mail → SMIPSecure = 'ssl';
+$mail → post = '465';
+$mail → setFrom =('maxxx899ttr@mail.ru')
+$mail → addAddress = ('maxxx999tte@gmail.com')
